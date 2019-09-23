de3c10952012343332b609f9d3155e5e49d69b14
Farhan Laeeq <far619@gmail.com>
2019-09-23 13:31:48 +0530

    Added UserRating.java

--- src/main/java/com/example/microservices/ratingsdataservice/controller/RatingsController.java
+++ src/main/java/com/example/microservices/ratingsdataservice/controller/RatingsController.java
@@ -1,10 +1,14 @@
 package com.example.microservices.ratingsdataservice.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microservices.ratingsdataservice.model.Rating;
+import com.example.microservices.ratingsdataservice.model.UserRating;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,6 +16,16 @@ public class RatingsController {
 
 	@RequestMapping("/{movieId}")
 	public Rating getRating(@PathVariable("movieId") String movieId) {
-		return new Rating(movieId,4);
+		return new Rating(movieId, 4);
 	}
+
+	@RequestMapping("users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
+
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratings);
+		return userRating;
+	}
+
 }
